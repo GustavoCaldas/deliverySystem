33dd7811cab3e402238cba4e96f2c88359c7c048
GustavoCaldas <gustavocaldes@gmail.com>
2021-01-27 21:30:56 -0300

    aula 3 1:12

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { fetchProducts } from '../api';
 import Footer from '../Footer';
+import { checkIsSelected } from './helpers';
 import OrderLocation from './OrderLocation';
 import OrderSummary from './OrderSummary';
 import ProductsList from './ProductsList'
@@ -10,20 +11,43 @@ import { OrderLocationData, Product } from './Types';
 
 function Orders() {
     const [products, setProducts] = useState<Product[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
     const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
+    const totalPrice = selectedProducts.reduce((sum, item) => {
+        return sum + item.price;
+    }, 0);
 
     useEffect(() => {
         fetchProducts()
         .then(response => setProducts(response.data))
         .catch(error => console.log(error))
-    }, [])
+    }, []);
+  
+    const handleSelectProduct = (product: Product) => {
+        const isAlreadySelected = checkIsSelected(selectedProducts, product)
+      
+        if (isAlreadySelected) {
+          const selected = selectedProducts.filter(item => item.id !== product.id);
+          setSelectedProducts(selected);
+        } else {
+          setSelectedProducts(previous => [...previous, product]);
+        }
+      }      
+
     return (
         <>
             <div className="orders-container">
                 <StepsHeader />
-                <ProductsList products={products}/>
+                <ProductsList 
+                    products={products} 
+                    onSelectProduct={handleSelectProduct} 
+                    selectedProducts={selectedProducts}
+                />
                 <OrderLocation onChangeLocation={location => setOrderLocation(location)}/>
-                <OrderSummary />
+                <OrderSummary 
+                    amount={selectedProducts.length} 
+                    totalPrice={totalPrice}
+                />
             </div>
             <Footer />
         </>
