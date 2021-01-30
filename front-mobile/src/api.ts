286d86579156399c6f2f3112e29cee742ed58b6d
Gustavo Caldas <gustavocaldes@gmail.com>
2021-01-30 13:31:45 -0300

    fix bug

--- front-mobile/src/api.ts
+++ front-mobile/src/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = 'https://deliverysystem-gus.herokuapp.com';
 
 export function fetchOrders() {
     return axios(`${API_URL}/orders`)
